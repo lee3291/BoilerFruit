0bea02f58382975f848dac7a36dc07807ed05a4f
Ethan <122648917+lee3291@users.noreply.github.com>
2023-04-23 21:35:18 -0400

    Feat: add welcome page panel

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,5 +1,48 @@
-public class ClientGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientGUI implements Runnable {
     public static void main(String[] args) {
-        DefaultFrame defaultFrame = new DefaultFrame("Hi", "");
+        SwingUtilities.invokeLater(new ClientGUI());
+    }
+
+    public void run() {
+        DefaultFrame dFrame = new DefaultFrame("Boiler Fruit Market");
+        dFrame.add(welcomePagePanel());
+        // dFrame.pack();
+        dFrame.setVisible(true);
+    }
+
+    public static JPanel welcomePagePanel() {
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(4, 0, 10, 10));
+
+        JLabel welcome = new JLabel("Welcome to Boiler Fruits");
+        welcome.setPreferredSize(new Dimension(100, 100));
+        welcome.setHorizontalAlignment(JLabel.CENTER);
+        welcome.setVerticalAlignment(JLabel.CENTER);
+        welcome.setFont(new Font("", Font.PLAIN, 20));
+
+        JLabel question = new JLabel("Are you a CUSTOMER or a SELLER?");
+        question.setPreferredSize(new Dimension(100, 100));
+        question.setHorizontalAlignment(JLabel.CENTER);
+        question.setVerticalAlignment(JLabel.NORTH);
+        question.setFont(new Font("", Font.PLAIN, 20));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0, 2, 10,20));
+        buttonPanel.setPreferredSize(new Dimension(100, 100));
+        JButton customerButton = new JButton("CUSTOMER");
+        customerButton.setSize(50, 50);
+        JButton sellerButton = new JButton("SELLER");
+        sellerButton.setSize(50,50);
+        buttonPanel.add(customerButton);
+        buttonPanel.add(sellerButton);
+
+        jPanel.add(welcome);
+        jPanel.add(question);
+        jPanel.add(buttonPanel);
+
+        return jPanel;
     }
 }
--- src/DefaultFrame.java
+++ src/DefaultFrame.java
@@ -1,17 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
- *  Class for default values set for our Jframes.
+ *  Class for default values set for our Jframe.
  */
 public class DefaultFrame extends JFrame {
-    public DefaultFrame(String title, String size) {
-
-        // Sizes will be promised, default or small.
-        // Default will be the basic frame size, small will be for error messages and other.
-
+    public DefaultFrame(String title) {
         this.setTitle(title);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(600, 400);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 }
