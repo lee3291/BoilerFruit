5ef6343a32230c347c5d217aab865652c105f3e9
Bao Quoc Phan <phan43@purdue.edu>
2023-04-27 02:58:05 -0400

    create the function signature for most of function in Server

--- src/Server.java
+++ src/Server.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Server implements Runnable {
+    User currentUser; // the client user
     Socket socket; // the client socket
     public final static Object sentinel = new Object(); // gatekeeper object for concurrency
     private static HashMap<String, User> users; // all the users
@@ -20,20 +21,264 @@ public class Server implements Runnable {
         this.socket = socket;
     }
 
-//    private boolean buyItem(String command, ObjectOutputStream output) {
-//        // do sth
-//        output.writeObject(new ArrayList<>());
-//    }
-
     /**
-     *
-     * @param command
-     * @param output
+     * Send an ArrayList of String containing the {@link #currentUser}'s purchase history
+     * @param output the output stream to communicate with client
      */
-    private void processCommand(String command, ObjectOutputStream output) {
+    private void getPurchaseHistory(ObjectOutputStream output) throws IOException {
 
     }
 
+    /**
+     * Decrease the specified product quantity by the specified purchase quantity
+     * Increase the Store's revenue
+     * Add the current {@link #currentUser} to the {@link Store}'s customer field
+     * Add the new sale history to the {@link Store}'s saleHistory field
+     * Add the new purchase history to the {@link #currentUser} purchaseHistory field
+     * @param output the output stream to communicate with client
+     * @param sellerEmail the store's sellerName (used for quicker searching)
+     * @param storeName the product's storeName
+     * @param productName the product's name
+     * @param purchaseQty the quantity purchase
+     */
+    private void buyItem(ObjectOutputStream output,
+                         String sellerEmail, String storeName, String productName, int purchaseQty) {
+
+    }
+
+    /**
+     * Loop through all Store in the Marketplace and collect all the Product
+     * @return an ArrayList of Product currently in the Marketplace
+     */
+    private ArrayList<Product> collectMarketProduct() {
+        ArrayList<Product> result = new ArrayList<>();
+
+        return result;
+    }
+
+    /**
+     * Get an ArrayList contains the Product from all existing Store within the Marketplace
+     * If searchKey is '-1', send an ArrayList of all Product in the Marketplace
+     * <p>
+     * If searchKey IS NOT '-1',
+     * send an ArrayList of Products that contain the searchKey in their name or description to client
+     * ArrayList is empty if there is no matching Product
+     * @param output the output stream to communicate with client
+     * @param searchKey the key to search (i.e. the product name or description); '-1' if no search is needed
+     */
+    private void getMarketProduct(ObjectOutputStream output, String searchKey) throws IOException {
+        ArrayList<Product> allProduct = collectMarketProduct();
+
+        // No search
+        if (searchKey.equals("-1")) {
+            output.writeObject(allProduct);
+        }
+
+        // Searching
+        else {
+            // TODO
+        }
+    }
+
+    /**
+     * Get an ArrayList contains the Product from all existing Store within the Marketplace
+     * Sort the ArrayList in the ascending order by price
+     * @param output the output stream to communicate with client
+     */
+    private void sortProductPrice(ObjectOutputStream output) throws IOException {
+        ArrayList<Product> allProduct = collectMarketProduct();
+    }
+
+    /**
+     * Get an ArrayList contains the Product from all existing Store within the Marketplace
+     * Sort the ArrayList in the ascending order by quantity
+     * @param output the output stream to communicate with client
+     */
+    private void sortProductQty(ObjectOutputStream output) throws IOException {
+        ArrayList<Product> allProduct = collectMarketProduct();
+    }
+
+    /**
+     * Remove the product from the specified store's product list
+     * Send a TRUE boolean object to client if success
+     * Send a FALSE boolean object to client if failed (i.e. the product does not exist in the specified store)
+     * @param output output the output stream to communicate with client
+     * @param sellerEmail the store's sellerName (used for quicker searching)
+     * @param storeName the name of the Store that contain the product
+     * @param productName the name of the product
+     */
+    private void deleteProduct(ObjectOutputStream output, String sellerEmail,
+                               String storeName, String productName) throws IOException {
+
+    }
+
+    /** TODO: Can we make sure price and quantity to be > 0 in GUI?
+     * Modify an existing product in the specified store, which exist in the {@link #currentUser}'s store field
+     * Send a TRUE boolean object to client if success
+     * Send a FALSE boolean object to client if failed (i.e. product's name is taken within the same store)
+     * @param output the output stream to communicate with client
+     * @param name the new product name
+     * @param description the description of the product
+     * @param price the price of the product
+     * @param quantity the quantity of the product
+     */
+    private void modifyProduct(ObjectOutputStream output,
+                            String name, String description, double price, int quantity) throws IOException {
+
+    }
+
+    /** TODO: Can we make sure price and quantity to be > 0 in GUI?
+     * Create a new product in the specified store, which exist in the {@link #currentUser}'s store field
+     * Send a TRUE boolean object to client if success
+     * Send a FALSE boolean object to client if failed (i.e. product's name is taken within the same store)
+     * @param output the output stream to communicate with client
+     * @param name the new product name
+     * @param storeName the product's store
+     * @param description the description of the product
+     * @param price the price of the product
+     * @param quantity the quantity of the product
+     */
+    private void addProduct(ObjectOutputStream output, String name, String storeName, String description,
+                            double price, int quantity) throws IOException {
+
+    }
+
+    /**
+     * Create an ArrayList contains the Product from the specified store
+     * If searchKey is '-1', send an ArrayList of all Product in the Store to client
+     * <p>
+     * If searchKey IS NOT '-1',
+     * send an ArrayList of Products that contain the searchKey in their name or description to client
+     * ArrayList is empty if there is no matching Product
+     * @param output the output stream to communicate with client
+     * @param sellerEmail the store's sellerName (used for quicker searching)
+     * @param searchKey the key to search (i.e. the product name or description); '-1' if no search is needed
+     */
+    private void getStoreProduct(ObjectOutputStream output, String sellerEmail,
+                                 String storeName, String searchKey) throws IOException {
+
+    }
+
+    /**
+     * Create an ArrayList contains all the Store from the {@link #currentUser}'s store field
+     * If searchKey is '-1', send an ArrayList of all Store to client
+     * <p>
+     * If searchKey IS NOT '-1',
+     * send an ArrayList of Store that contain the searchKey in their name to client
+     * ArrayList is empty if there is no matching Store
+     * @param output the output stream to communicate with client
+     * @param searchKey the key to search (i.e. the store name); '-1' if no search is needed
+     */
+    private void getSellerStores(ObjectOutputStream output, String searchKey) throws IOException {
+
+    }
+
+    /**
+     * Delete a store in the {@link #currentUser}'s stores field
+     * Send a TRUE boolean object to client if success
+     * Send a FALSE boolean object to client if failed (i.e. there is no store with matching name)
+     * @param output the output stream to communicate with client
+     * @param storeName the name of the store to be deleted
+     */
+    private void deleteStore(ObjectOutputStream output, String storeName) throws IOException {
+
+    }
+
+    /**
+     * Create a new store in the {@link #currentUser}'s stores field
+     * Send a TRUE boolean object to client if success
+     * Send a FALSE boolean object to client if failed (i.e. seller also has the store with similar name)
+     * @param storeName the new store's name
+     * @param output the output stream to communicate with client
+     */
+    private void createStore(ObjectOutputStream output, String storeName) throws IOException {
+
+    }
+
+    /**
+     * Send a string object containing the {@link #currentUser}'s username to the client
+     * @param output the output stream to communicate with client
+     */
+    private void getUsertype(ObjectOutputStream output) throws IOException {
+
+    }
+
+    /**
+     * Send a string object containing the {@link #currentUser}'s username to the client
+     * @param output the output stream to communicate with client
+     */
+    private void getUsername(ObjectOutputStream output) throws IOException {
+
+    }
+
+    /**
+     * Delete the user in {@link #users} associate with the given email
+     * @param email the email of the account to be deleted
+     */
+    private void deleteAccount(String email) {
+
+    }
+
+    /**
+     * Validate the username, and password; TODO: Can this be done in the GUI?
+     * Modify the {@link #users}'s username and password
+     * Send a TRUE boolean object to client if success;
+     * Send a FALSE boolean object to client if failed (i.e. username is taken)
+     * @param output the output stream to communicate with client
+     * @param username the username of the new user
+     * @param password the password of the new user
+     */
+    private void modifyAccount(ObjectOutputStream output, String username, String password) throws IOException {
+
+    }
+
+    /**
+     * Close the socket
+     */
+    private void logOut() throws IOException {
+        socket.close();
+    }
+
+    /**
+     * Find the user with a matching id (username or email) and email
+     * Set the {@link #currentUser} to the newly created User if success;
+     * Send a TRUE boolean object to client if success;
+     * Send a FALSE boolean object to client if failed.
+     * @param output the output stream to communicate with client
+     * @param id username or email of the client
+     * @param password password of the client
+     */
+    private void logIn(ObjectOutputStream output, String id, String password) throws IOException {
+
+    }
+
+    /**
+     * Validate the username, email, and password; TODO: Can this be done in the GUI?
+     * Create a new user in {@link #users} and set the {@link #currentUser} to the newly created User if success;
+     * Send a  1 integer object to client if success;
+     * Send a  0 integer object to client if email is taken
+     * Send a -1 integer object to client if username is taken)
+     * @param output the output stream to communicate with client
+     * @param username the username of the new user
+     * @param email the email of the new user
+     * @param password the password of the new user
+     */
+    private void signUp(ObjectOutputStream output, String username, String email, String password) throws IOException {
+
+    }
+
+    /**
+     * Process the command get from the Client GUI and call the appropriate method with appropriate parameters
+     * @param command the command from the Client
+     * @param output the ObjectOutputStream to send object to client
+     */
+    private void processCommand(String command, ObjectOutputStream output) throws IOException {
+
+    }
+
+    /**
+     * The run method for Concurrency
+     */
     public void run() {
         System.out.printf("Connection received from %s\n", socket);
         try (Scanner input = new Scanner(socket.getInputStream())) {
